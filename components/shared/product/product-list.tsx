c417d5e2e8fb0264a9a8710335f0b542437a01fd
Rana <reduanulislam92665@gmail.com>
2025-02-22 10:53:17 +0600

    single  product page made

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,11 +1,12 @@
 import ProductCard from "./product-card";
-const ProductList = ({data, title, limit}:{data:any; title?:string; limit?:number}) => {
+import { Product } from "@/types";
+const ProductList = ({data, title, limit}:{data:Product[]; title?:string; limit?:number}) => {
       const limitData = limit ? data.slice(0,limit) : data
       return ( <div className="my-10">
             <h2 className="h2-bold mb-4">{title}</h2>
             {data.length > 0 ? (
                   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-                        {limitData.map((product: any, index:number)=>(
+                        {limitData.map((product: Product, index:number)=>(
                               <ProductCard key={product.slug} product={product} ></ProductCard>
                         ))}
                   </div>
